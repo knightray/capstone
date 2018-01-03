3887df9509e5d8e79a56f1ddb8de6babcfdd9922
章承科 <zhangchengke@zhangchengkedeMacBook-Pro.local>
2018-01-03 17:14:32 +0800

    Add define

---
+++ src/define.py
@@ -0,0 +1,13 @@
+#
+# Common definition
+# 
+# by ZhangChengKe
+# 2017.12.28
+# =======================================================
+
+IMAGE_W = 200
+IMAGE_H = 200
+N_CLASSES = 2
+DATA_DIR = '../data/oxford-pet/'
+LOG_DIR = '../logs/'
+
