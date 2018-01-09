b626f3596e1d92206eddac82c87963e80b37dd18
章承科 <zhangchengke@zhangchengkedeMacBook-Pro.local>
2018-01-09 22:09:07 +0800

    modified for kaggle dataset

--- src/define.py
+++ src/define.py
@@ -8,6 +8,12 @@
 IMAGE_W = 200
 IMAGE_H = 200
 N_CLASSES = 2
-DATA_DIR = '../data/oxford-pet/'
+DATA_DIR = '../data/kaggle/'
 LOG_DIR = '../logs/'
+CAT = 0
+DOG = 1
+TRAINING_IMAGE_PERCENT = 0.8
+BATCH_SIZE = 8
+MAX_STEP = 10000
+LEARNING_RATE = 0.0001
 
