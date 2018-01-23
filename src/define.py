6e74974bf5ca1d99df2b8f8bd8ea42604d6dde62
章承科 <zhangchengke@zhangchengkedeMacBook-Pro.local>
2018-01-23 18:34:12 +0800

    add epoch trainning

--- src/define.py
+++ src/define.py
@@ -5,15 +5,37 @@
 # 2017.12.28
 # =======================================================
 
+#
+# Image definition
+#
 IMAGE_W = 200
 IMAGE_H = 200
-N_CLASSES = 2
+
+#
+# Directory definition
+#
 DATA_DIR = '../data/kaggle/'
 LOG_DIR = '../logs/'
+
+#
+# Classify definition
+#
+N_CLASSES = 2
 CAT = 0
 DOG = 1
+
+#
+# Trainning definition
+#
 TRAINING_IMAGE_PERCENT = 0.8
+N_EPOCH = 10
 BATCH_SIZE = 8
-MAX_STEP = 10000
 LEARNING_RATE = 0.0001
 
+# 
+# Test definition
+#
+TYPE_ONE_IMAGE = "image"
+TYPE_VERIFY_SET = "verify"
+TYPE_TEST_SET = "test"
+TYPE_EPOCH_VERIFY_SET = "epoch"
