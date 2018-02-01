941bdfaf76b83c660f318370fb39e53402e31fce
章承科 <zhangchengke@zhangchengkedeMacBook-Pro.local>
2018-02-01 22:31:30 +0800

    add train_and_verify

--- src/training.py
+++ src/training.py
@@ -15,6 +15,72 @@ from model import get_model
 
 FLAGS = None
 
+def trainning_and_verify(train_images, train_labels, verify_images, verify_labels):
+
+	model = get_model()
+	train_image_batch, train_label_batch = data_processing.get_batches(train_images, train_labels, define.BATCH_SIZE, define.IMAGE_W, define.IMAGE_H)
+	verify_image_batch, verify_label_batch = data_processing.get_batches(verify_images, verify_labels, define.BATCH_SIZE, define.IMAGE_W, define.IMAGE_H)
+	
+	#print("image_batch=%s, label_batch=%s" % (image_batch.shape, label_batch))
+	x = tf.placeholder(tf.float32, shape = [define.BATCH_SIZE, define.IMAGE_W, define.IMAGE_H, 3], name = "x")
+	y = tf.placeholder(tf.int32, shape = [define.BATCH_SIZE], name = "y")
+
+	train_logits = model.inference(x, define.N_CLASSES)
+	train_loss = model.losses(train_logits, y)
+	train_op = model.trainning(train_loss, define.LEARNING_RATE)
+	train_acc_op = model.evaluation(train_logits, y)
+
+	logs_dir = vars(FLAGS)['log_dir']
+	if (vars(FLAGS)['max_step'] != 0):
+		max_step = vars(FLAGS)['max_step']
+	else:
+		max_step = int(len(train_images) / define.BATCH_SIZE)
+	
+	define.log("Do trainning for %d step in one epoch." % max_step)
+	if not os.path.exists(logs_dir):
+		os.mkdir(logs_dir)
+
+	summary_op = tf.summary.merge_all()
+	sess = tf.Session()
+	train_writer = tf.summary.FileWriter(logs_dir, sess.graph)
+	saver = tf.train.Saver(max_to_keep=define.N_EPOCH)
+
+	sess.run(tf.global_variables_initializer())
+	coord = tf.train.Coordinator()
+	threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+	try:
+		for epoch in range(define.N_EPOCH):
+			for step in range(max_step):
+				if coord.should_stop():
+					break
+				tra_images, tra_labels = sess.run([train_image_batch, train_label_batch])
+				_, tra_loss, tra_acc = sess.run([train_op, train_loss, train_acc_op], feed_dict = {x:tra_images, y:tra_labels})
+
+				if step % 100 == 0:
+					define.log(' Step %d, train loss = %.2f, train accuracy = %.2f%%' %(step, tra_loss, tra_acc*100.0))
+					summary_str = sess.run(summary_op, feed_dict = {x:tra_images, y:tra_labels})
+					train_writer.add_summary(summary_str, step)
+
+			define.log(' END Step %d, train loss = %.2f, train accuracy = %.2f%%' %(step, tra_loss, tra_acc*100.0))
+			checkpoint_path = os.path.join(logs_dir, 'model_%s.ckpt' % define.USE_MODEL)
+			saver.save(sess, checkpoint_path, global_step=(epoch + 1)*max_step)
+
+			define.log(" Verify at verify set...")
+			verify_images, verify_labels = sess.run([verify_image_batch, verify_label_batch])
+			_, verify_loss, verify_acc = sess.run([train_op, train_loss, train_acc_op], feed_dict={x:verify_images, y:verify_labels})
+			define.log(' verify loss = %.2f, verify accuracy = %.2f%%' %(verify_loss, verify_acc*100.0))
+
+			define.log("**** EPOCH %d FINISHED ****" % (epoch + 1)) 
+
+	except tf.errors.OutOfRangeError:
+		define.log('Done training -- epoch limit reached')
+	finally:
+		coord.request_stop()
+
+	coord.join(threads)
+	sess.close()
+
 def training(images, labels):
 
 	model = get_model()
@@ -83,7 +149,8 @@ def main(_):
 	test_data_list = log_dir + '/test_list.csv'
 	data_processing.save_list(test_images_list, test_labels_list, test_data_list)
 
-	training(train_images_list, train_labels_list)
+	#training(train_images_list, train_labels_list)
+	trainning_and_verify(train_images_list, train_labels_list, test_images_list, test_labels_list)
 
 
 if __name__ == '__main__':
